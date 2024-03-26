b7d942f1a1c39ba12a1e9f4a3cecd5210f3b4558
alla.sultan <alla.sultan@isa.utm.md>
2024-03-26 22:37:52 +0200

    Domsain & Service commit

---
+++ GS-Parfum/GS-Parfum.Domain/Response/BaseResponse.cs
@@ -0,0 +1,21 @@
+﻿using GS_Parfum.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GS_Parfum.Domain.Response
+{
+    public class BaseResponse<T> : IBaseRepsonse<T>
+    {
+        public string Description { get; set; }
+        public ResponseStatus ResponseStatus { get; set; }
+        public T Data { get; set; }
+    }
+
+    public interface IBaseRepsonse<T>
+    {
+        T Data { get; set; }
+    }
+}
